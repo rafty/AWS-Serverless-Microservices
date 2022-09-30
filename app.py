991dcc9c558f94a7714a682ea281726563bf3102
takashi <yagita.takashi@gmail.com>
2022-09-30 18:24:34 +0900

    First commit

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python3
 import os
-
 import aws_cdk as cdk
+from stacks.product_service_stack import ProductServiceStack
+from stacks.basket_service_stack import BasketServiceStack
+from stacks.order_service_stack import OrderServiceStack
 
-from aws_serverless_microservices.aws_serverless_microservices_stack import AwsServerlessMicroservicesStack
 
+env = cdk.Environment(
+    account=os.environ.get("CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]),
+    region=os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"]),
+)
 
 app = cdk.App()
-AwsServerlessMicroservicesStack(app, "AwsServerlessMicroservicesStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+ProductServiceStack(app, "ProductServiceStack", env=env)
+BasketServiceStack(app, "BasketServiceStack", env=env)
+OrderServiceStack(app, "OrderServiceStack", env=env)
 
 app.synth()
